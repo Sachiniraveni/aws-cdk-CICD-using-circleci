feb47f8830b064ca74f32024fae29c79131809ce
code_.blooded <46579657+Sachiniraveni@users.noreply.github.com>
2021-08-18 12:17:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from aws_cdk import core
 
 from cdk_sample.cdk_sample_stack import CdkSampleStack
 
+# with open('config/env_lower.yaml') as f:
+#     data = yaml.load(f, Loader=yaml.FullLoader)
+
 
 app = core.App()
 CdkSampleStack(app, "cdk-sample")
